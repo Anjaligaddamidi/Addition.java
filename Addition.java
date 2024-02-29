ce5de452b5565253d2bab2a53f019cb163cd58fe
Anjaligaddamidi <149222107+Anjaligaddamidi@users.noreply.github.com>
2024-02-29 12:58:51 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,9 @@
+class Addition
+{
+public static void main(String[] args)
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+System.out.println("addition of"+a+"+"+b+"is"+(a+b));
+}
+}
